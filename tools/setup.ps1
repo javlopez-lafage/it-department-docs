3d94540e8a4ff49d190a3f6b273baebdaa227a63
javlopez-lafage <jalopez@lab-lafage.com>
2026-02-18 09:25:12 -0600

    chore(setup): agregar script setup.ps1 para configuración automátic
    Se agrega script setup.ps1 que permite el uso de plantilla para que cada commit utilice los estándares de mensajes de commit

---
+++ tools/setup.ps1
@@ -0,0 +1,7 @@
+# Configurar plantilla de commits
+git config commit.template docs/commit-template.txt
+
+# Configurar VS Code como editor por defecto
+git config core.editor "code --wait"
+
+Write-Host "✅ Configuración aplicada: plantilla y editor listos."
